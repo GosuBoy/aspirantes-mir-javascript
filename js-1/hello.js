338e2009d90b9eb63579c160fcda80efc3d7e71b
Javier_Chavez <david.o66@hotmail.com>
2023-03-10 18:39:57 -0500

    Added solution to hello.js

---
+++ js-1/hello.js
@@ -0,0 +1,10 @@
+// escribe la función hola acá
+function hola($name){
+    var _message = "" ;
+    _message = "Hola " + $name + "!"
+    return _message ;
+}
+// código de prueba
+console.log(hola("Pedro")) // "Hola Pedro!"
+console.log(hola("Juan")) // "Hola Juan!"
+console.log(hola("")) // "Hola !"
