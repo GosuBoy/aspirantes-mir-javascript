99e8352fe7a5647585d04708ad941b8842bd74d5
Javier_Chavez <david.o66@hotmail.com>
2023-03-10 18:51:13 -0500

    Added solution to sum.js

--- js-1/sum.js
+++ js-1/sum.js
@@ -1,5 +1,11 @@
 // escribe la función suma acá
-
+function sum ( $number ) {
+    var $result = 0 ;
+    for ( let i = 1 ; i <= $number ; i++ ){
+        $result += i ;
+    }
+    return $result;     
+}
 // código de prueba
 console.log(sum(4)) // 1 + 2 + 3 + 4 = 10
 console.log(sum(10)) // 55
