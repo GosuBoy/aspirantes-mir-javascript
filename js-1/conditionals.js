1bcb19873fc48f001f0f79b1520e0383b587ba11
Javier_Chavez <david.o66@hotmail.com>
2023-03-10 18:58:43 -0500

    Added solutions to all js exercises

--- js-1/conditionals.js
+++ js-1/conditionals.js
@@ -1,15 +1,14 @@
-function calcuarColor() {
+function calcularColor(num) {
     var color;
     if (num === 1) {
-      color = "black";
-    } else (num = 2) {
-      color = "blanco":
-    } else if (num === 3) {
+      color = "negro";
+    } else if (num === 2) {
+      color = "blanco" ;
+    } else  if(num === 3) {
       color = "rojo";
     } else {
-      color: "verde";
+      color = "verde";
     }
-  
     return "El color es " + color;
   }
   
